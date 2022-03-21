36addba703079420cafed00650a1dc0edb78f859
Daegeun Yoon <krdaegeun@gmail.com>
2022-03-21 14:33:26 +0900

    feature : basic paging for all boards by id

--- src/main/java/pj/com/repository/BoardRepository.java
+++ src/main/java/pj/com/repository/BoardRepository.java
@@ -1,9 +1,13 @@
 package pj.com.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pj.com.model.Board;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    Page<Board> findAllByOrderByIdDesc(Pageable pageable);
 }
